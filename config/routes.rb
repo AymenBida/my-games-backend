7736dd6b6c78ab261085db12b1e85a7ca5ce8d88
AymenBida <aymen.bida.00@gmail.com>
2021-07-29 11:37:35 +0100

    add LOGIN controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :games, only: [:index, :show, :create, :update, :destroy]
   post '/signup', to: 'users#create'
+  post '/login', to: 'authentication#authenticate'
 end
