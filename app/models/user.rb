9eb275aadb2af464607085a5c87c7bcc4f8cc31e
AymenBida <aymen.bida.00@gmail.com>
2021-07-29 10:05:03 +0100

    Add uniqueness validation to users emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :favourites
   validates_presence_of :name, :email, :password_digest
+  validates :email, uniqueness: true
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let(:valid_attributes) do
+    {
+      name: 'Aymen',
+      email: 'aymen@bida.com',
+      password: '123456'
+    }
+  end
+
+  describe 'POST /signup' do
+    before 
+  end
+end
