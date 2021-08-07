5f6e9afdc7c9be4f1a6094fc61b2bad996e58a84
AymenBida <aymen.bida.00@gmail.com>
2021-08-07 12:42:13 +0100

    Provide username when logged in

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -5,7 +5,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    { auth_token: JsonWebToken.encode(user_id: user.id), username: user.name } if user
   end
 
   private
